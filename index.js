ef1686ac1619c478e2f05b6fc47b82c03a822e45
Nikolas <nkls99@gmail.com>
2022-04-22 19:28:23 -0300

    get last 10 tweets enabled

--- index.js
+++ index.js
@@ -7,19 +7,29 @@ app.use(cors());
 
 let users = [];
 let tweets = [];
-let postedTweets = [];
-
 app.post("/sign-up", (req, res) => {
     users.push(req.body);
-    res.send("OK");
-});
-
-app.post("/tweets", (req, res) => {
-    tweets.push(req.body);
     res.send("OK");
     console.log(req.body);
 });
 
+app.post("/tweets", (req, res) => {
+    const userThatTweeted = users.find(user => user.username === req.body.username);
+    tweets.push(
+        {
+            username: req.body.username,
+            avatar: userThatTweeted.avatar, 
+            tweet: req.body.tweet
+        }
+    );
+    res.send("OK");
+    console.log(req.body);
+});
 
+app.get("/tweets", (req, res) => {
+    const lastTenTweets = tweets.filter((tweet, index) => index > tweets.length - 11)
+    res.send(lastTenTweets);
+    console.log("foi");
+});
 
 app.listen(5000, () => console.log("server online"));
