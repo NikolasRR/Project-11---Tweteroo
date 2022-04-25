03584807c0fac3cb642cdd86c5b0dedaa2e71688
Nikolas <nkls99@gmail.com>
2022-04-25 18:25:41 -0300

    corrigi a ordem de exibicao dos tweets, como isso n era um requisito, acredito que n tem problema =D

--- index.js
+++ index.js
@@ -10,12 +10,11 @@ let tweets = [];
 app.post("/sign-up", (req, res) => {
     users.push(req.body);
     res.send("OK");
-    console.log(req.body);
 });
 
 app.post("/tweets", (req, res) => {
     const userThatTweeted = users.find(user => user.username === req.body.username);
-    tweets.push(
+    tweets.unshift(
         {
             username: req.body.username,
             avatar: userThatTweeted.avatar, 
@@ -23,13 +22,12 @@ app.post("/tweets", (req, res) => {
         }
     );
     res.send("OK");
-    console.log(req.body);
 });
 
 app.get("/tweets", (req, res) => {
-    const lastTenTweets = tweets.filter((tweet, index) => index > tweets.length - 11)
+    let lastTenTweets = [];
+    lastTenTweets = tweets.filter((tweet, index) => index < 10);
     res.send(lastTenTweets);
-    console.log("foi");
 });
 
 app.listen(5000, () => console.log("server online"));
